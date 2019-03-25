c0d3a94e2e3ee83d5234c446f6539378caf2a87f
Alaattin Cafer Kaynak <ByCaFCaF@sagocuyuz.com>
2019-03-25 06:52:47 +0300

    Revert "Revert "Commit Staged""
    This reverts commit 4b88ecb3d901ea29c93376bf28d5f545cc628d1c.

--- bookbooking/bookbooking.Common/ViewModels/Library/BookView.cs
+++ bookbooking/bookbooking.Common/ViewModels/Library/BookView.cs
@@ -7,5 +7,6 @@ namespace bookbooking.Common.ViewModels.Library
     {
         public List<Book> Books { get; set; }
         public Book Book { get; set; }
+        public List<Entity.Entities.Category> categories { get; set; }
     }
 }
--- bookbooking/bookbooking.Service/LibraryService.cs
+++ bookbooking/bookbooking.Service/LibraryService.cs
@@ -1,17 +1,35 @@
-﻿using bookbooking.Common.ViewModels.Library;
+﻿using bookbooking.Common.ViewModels;
+using bookbooking.Common.ViewModels.Library;
 using bookbooking.Data;
 using bookbooking.Entity.Entities;
+using bookbooking.Service.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace bookbooking.Service
 {
-    public class LibraryService
+    public interface ILibraryService
+    {
+        BookView BookList();
+        BookView UpdateBookList(int id);
+        Task<ServiceResult> AddBook(BookView model,IFormFile file);
+        Task<ServiceResult> UpdateBook(BookView model, IFormFile file);
+        void RemoveBook(int id);
+    }
+
+    public class LibraryService : ILibraryService
     {
         private IRepository<Book> bookRepository;
         private IRepository<Category> categoryRepository;
+        ServiceResult serviceResult = new ServiceResult();
+        BookView bookView = new BookView();
         public LibraryService(IRepository<Book> _bookRepository, IRepository<Category> _categoryRepository)
         {
             bookRepository = _bookRepository;
@@ -19,24 +37,50 @@ namespace bookbooking.Service
         }
         public BookView BookList()
         {
-            BookView bookView = new BookView();
-            List<Category> category = new List<Category>();
-            category = categoryRepository.GetAll().ToList();
-            bookView.Books = bookRepository.GetAll().ToList();
-
+            
+            bookView.Books = bookRepository.GetAll().Include(i => i.Category).ToList();
+            bookView.categories = categoryRepository.GetAll().ToList();
             return bookView;
         }
-        public void AddBook(BookView model)
+        public BookView UpdateBookList(int id)
         {
-            bookRepository.Add(model.Book);
+            bookView.Book = bookRepository.GetAll().Where(w => w.Id == id).FirstOrDefault();
+            bookView.Books = bookRepository.GetAll().Include(i => i.Category).ToList();
+            bookView.categories = categoryRepository.GetAll().ToList();
+            return bookView;
         }
-        public void UpdateBook(BookView model)
+        public async Task<ServiceResult> AddBook(BookView model, IFormFile file)
         {
+            if (file != null)
+            {
+                CurrentDirectoryHelpers.SetCurrentDirectory();
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Image", file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                    model.Book.ImageName = file.FileName;
+                    serviceResult.sonuc = true;
+                }
+            }
+                bookRepository.Add(model.Book);
+            return serviceResult;
+        }
+        public async Task<ServiceResult> UpdateBook(BookView model, IFormFile file)
+        {
+            CurrentDirectoryHelpers.SetCurrentDirectory();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Image", file.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+                model.Book.ImageName = file.FileName;
+                serviceResult.sonuc = true;
+            }
             bookRepository.Update(model.Book);
+            return serviceResult;
         }
-        public void RemoveBook(BookView model)
+        public void RemoveBook(int id)
         {
-            bookRepository.Remove(model.Book);
+            bookRepository.Remove(bookRepository.GetAll().Where(w => w.Id == id).FirstOrDefault());
         }
     }
 }
